7acb6522a84879d8bb36b34c33754e3407994265
zplll <1418641603@qq.com>
2017-03-06 23:59:15 +0800

    create  table sql

---
+++ src/main/java/houji/bean/UserInfo.java
@@ -0,0 +1,76 @@
+package houji.bean;
+
+import java.io.Serializable;
+
+/**
+ * Created by Administrator on 2017/3/6.
+ */
+public class UserInfo implements Serializable {
+
+    private int userId;
+    private String name;
+    private String password;
+    private String email;
+    private int roleId;
+
+
+    public UserInfo() {
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("{");
+        sb.append("\"userId\":")
+                .append(userId);
+        sb.append(",\"name\":\"")
+                .append(name).append('\"');
+        sb.append(",\"password\":\"")
+                .append(password).append('\"');
+        sb.append(",\"email\":\"")
+                .append(email).append('\"');
+        sb.append(",\"roleId\":")
+                .append(roleId);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+}
---
+++ src/main/java/houji/houji.java
@@ -0,0 +1,7 @@
+package houji;
+
+/**
+ * Created by Administrator on 2017/3/6.
+ */
+public class houji {
+}
